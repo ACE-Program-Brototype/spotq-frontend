80e3ce46085bdb2d831b15b2cf23c9ff757e2a0a
Surynx <sooryanarayanan1082004@gmail.com>
2026-08-15 18:09:20 +0530

    feat: setup TanStack Query provider

--- src/app/provider/AppProviders.tsx
+++ src/app/provider/AppProviders.tsx
@@ -1,8 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import router from "@/app/router/routes";
+import QueryProvider from "@/app/provider/QueryProvider";
 
 function AppProviders() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryProvider>
+        <RouterProvider router={router} />
+    </QueryProvider>
+  );
 }
 
 export default AppProviders;
