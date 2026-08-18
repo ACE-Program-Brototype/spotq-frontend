0d99d270860a9754764b36fa0a098ca22b6036a0
Sooryanarayanan <154858339+Surynx@users.noreply.github.com>
2026-08-18 17:40:28 +0530

    Feature/scrum 553 frontend basic setup (#2)
    * refactor: restructure frontend folder architecture
    
    * refactor: migrate to React Router v7
    
    * feat: setup TanStack Query provider
    
    * refactor: replace axios with ky
    
    * feat: initialize authentication store using zustand lib
    
    * chore:  added react-hook-form zod sonner lucide-react dependencies
    
    * chore: configured tailwindcss
    
    * chore: configured shadcn
    
    * style: added a button to check the shadcn
    
    * feat: added Ky interceptor hooks
    
    * docs: updated the folder structure and tech stack
    
    * feat: use Ky prefixUrl for API client
    
    * refactor: updated the api_url not found message
    
    * refactor: moved env injection to seperate config file

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -3,17 +3,11 @@ import { afterResponse } from "./hooks/afterResponse";
 import { beforeError } from "./hooks/beforeError";
 import { beforeRequest } from "./hooks/beforeRequest";
 import { beforeRetry } from "./hooks/beforeRetry";
+import env from "@/config/env";
 
-const rawApiUrl = import.meta.env.VITE_API_BASE_URL;
-
-if (!rawApiUrl) {
-  throw new Error("VITE_API_BASE_URL is not configured.");
-}
-
-const API_URL = /^\d+$/.test(rawApiUrl) ? `http://localhost:${rawApiUrl}` : rawApiUrl;
 
 export const apiClient = ky.create({
-  baseUrl: API_URL,
+  prefix: env.apiUrl,
   timeout: 10_000,
   credentials: "include",
   retry: {
