99d044550d205edec8b25fcfc32af7468bdc46e5
Surynx <sooryanarayanan1082004@gmail.com>
2026-09-09 16:41:48 +0530

    feat: added proper routing for onboarding

--- src/app/router/restaurant.routes.tsx
+++ src/app/router/restaurant.routes.tsx
@@ -1,13 +1,12 @@
-import type { RouteObject } from "react-router-dom";
-
+import { type RouteObject } from "react-router-dom";
 import RestaurantDashboardPage from "@/features/auth/pages/RestaurantDashboardPage";
 import RestaurantEmailVerificationPage from "@/features/auth/pages/RestaurantEmailVerification";
-import RestaurantOnboardingPage from "@/features/auth/pages/RestaurantOnboardingPage";
 import RestaurantTermsPage from "@/features/auth/pages/RestaurantTermsPage";
 import OtpVerificationPage from "@/features/auth/pages/ResturantOtpVerification";
 import AuthLayout from "@/layouts/AuthLayout";
 import ProtectedLayout from "@/layouts/ProtectedLayout";
 import RestaurantAdminLayout from "@/layouts/RestaurantAdminLayout";
+import OnboardLayout from "@/layouts/OnboardLayout";
 
 const RestaurantAuthLayout = () => <AuthLayout redirectTo="/restaurant/dashboard" />;
 const RestaurantProtectedLayout = () => (
@@ -37,9 +36,34 @@ export const restaurantRoutes: RouteObject[] = [
         path: "otp/verification",
         Component: OtpVerificationPage,
       },
+
       {
         path: "onboarding",
-        Component: RestaurantOnboardingPage,
+        Component: OnboardLayout,
+        children: [
+          // {
+          //   index: true,
+          //   Component: () => (
+          //     <Navigate to="business-information" replace />
+          //   ),
+          // },
+          // {
+          //   path: "business-information",
+          //   Component: BusinessInformationPage,
+          // },
+          // {
+          //   path: "documents",
+          //   Component: DocumentsPage,
+          // },
+          // {
+          //   path: "location",
+          //   Component: LocationPage,
+          // },
+          // {
+          //   path: "review",
+          //   Component: ReviewPage,
+          // },
+        ],
       },
     ],
   },
--- src/layouts/OnboardLayout.tsx
+++ src/layouts/OnboardLayout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import OnboardSidebar from "@/features/onboard/components/Onboardsidebar";
 
-
 export default function OnboardLayout() {
   return (
     <div className="min-h-screen bg-neutral-100">
@@ -20,7 +19,6 @@ export default function OnboardLayout() {
         </div>
       </header>
 
-    
       <div className="mx-auto flex max-w-6xl flex-col gap-10 px-6 py-10 sm:px-10 lg:flex-row">
         <OnboardSidebar />
 
